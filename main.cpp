8515b43cabc044c126b806488f55ddd3c3c54568
plebian_tier <ddethmo@gmail.com>
2018-11-12 11:01:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,56 @@
 #include<math.h>
 #include<ctype.h>
 
-#define MAXLENGTH 100
-
+#define MAXLENGTH 150
+#define MAXCOLUMNS 2
+/*	Reads in a file of integers to store into a 2d array
+	Must be seperated by a whitespace for each column
+	of max size 100,2
+*/
+//returns the min of the numbers in each column
+int min(int num[]){
+	int min=num[0];
+	for(int x=0;x<sizeof(num);x++){
+		if(num[x]<min){
+			min=num[x];
+		}
+	}
+	return min;
+}
+//returns the max of the numbers in each column
+int max(int num[]){
+	int max=num[0];
+	for(int x=0;x<sizeof(num);x++){
+		if(num[x]>max){
+			max=num[x];
+		}
+	}
+	return max;
+}
+//average of each column
+double mean(int num[]){
+	int mean=0;
+	for(int x=0;x<sizeof(num);x++){
+		mean+=num[x];
+	}
+	return mean;
+}
+//standard deviation of each column
+double stdev(){
+	return 0;
+}
+//runs the program, asks for a file location
 int main(){
 	FILE *fp;
+//must be less than MAXLENGTH value
 	char filename[MAXLENGTH];
 	bool isfilename=false;
+
 	do{
+		//asks for a filename then gives true or false based on whether file exists
 		printf("Gimme a file name: ");
 		scanf("%s",&filename);
 		fp=fopen(filename,"r");
-		//asks for a filename then gives true or false based on whether file exists
 		if(fp){
 			printf("Your file exists\n");
 			isfilename=true;
@@ -24,14 +63,23 @@ int main(){
 			printf("Your file does not exist\n");
 		}
 	}while(fp==false);
+
 //when file is true creates a 2d array here
 //where x=rows and y= column of data
-	int *numbs[MAXLENGTH][MAXLENGTH];
+	int numbs[MAXLENGTH][MAXLENGTH];
 	for(int x=0;x<MAXLENGTH;x++){
-		for(int y=0; y<2;y++){
-			fscanf(fp,"%d",numbs[x][y]);
+		for(int y=0; y<MAXCOLUMNS;y++){
+			fscanf(fp,"%d",&numbs[x][y]);
 		}
 	}
+
+	for(int y=0;y<MAXCOLUMNS;y++){
+		//create 1d arrays from a 2d a array here
+		printf("%d",mean(numbs));
+	}
+
+
 	fclose(fp);
+	system("pause");
 	return 0;
 }
