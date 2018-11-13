efba2170a522eeb7a3e964056d0ffdd6026d19f5
plebian_tier <ddethmo@gmail.com>
2018-11-13 10:40:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,85 +1,137 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<math.h>
 #include<ctype.h>
+#include<string.h>
 
-#define MAXLENGTH 150
-#define MAXCOLUMNS 2
-/*	Reads in a file of integers to store into a 2d array
-	Must be seperated by a whitespace for each column
-	of max size 100,2
-*/
-//returns the min of the numbers in each column
-int min(int num[]){
-	int min=num[0];
-	for(int x=0;x<sizeof(num);x++){
-		if(num[x]<min){
-			min=num[x];
-		}
-	}
-	return min;
+
+void function1(){
+	printf("Group number: 33\nCreators: Daniel Dethero, Trevor Ross, and Christabel Mbaba\n\n11/12/18\nPurpose is to add, subtract, multiply, or divide these two numbers and display the results to the screen as well as a file");
 }
-//returns the max of the numbers in each column
-int max(int num[]){
-	int max=num[0];
-	for(int x=0;x<sizeof(num);x++){
-		if(num[x]>max){
-			max=num[x];
-		}
-	}
-	return max;
+int function2(int pos){
+	int num;
+	printf("\nEnter a value for number %d: ",pos);
+	scanf("%d",&num);
+	return num;
 }
-//average of each column
-double mean(int num[]){
-	int mean=0;
-	for(int x=0;x<sizeof(num);x++){
-		mean+=num[x];
-	}
-	return mean;
-}
-//standard deviation of each column
-double stdev(){
-	return 0;
-}
-//runs the program, asks for a file location
-int main(){
-	FILE *fp;
-//must be less than MAXLENGTH value
-	char filename[MAXLENGTH];
-	bool isfilename=false;
+int function3(){
+	int choice=0;
+	bool moron=false;
 
 	do{
-		//asks for a filename then gives true or false based on whether file exists
-		printf("Gimme a file name: ");
-		scanf("%s",&filename);
-		fp=fopen(filename,"r");
-		if(fp){
-			printf("Your file exists\n");
-			isfilename=true;
+		if(moron){
+			printf("That's not one of the options :), please enter a correct number\n");
 		}
-		else{
-			printf("Your file does not exist\n");
-		}
-	}while(fp==false);
+		printf("Whatcha wanna do with yer numbers m8y\nType in one of the following numbers\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n");
+		scanf("%d",&choice);
+		moron=true;
+	}while(choice>4||choice<1);
+	return choice;
+}
+double function4(int userchoice,int num1,int num2){
+	switch(userchoice){
+		case 1:
+		return num1+num2;
+		break;
 
-//when file is true creates a 2d array here
-//where x=rows and y= column of data
-	int numbs[MAXLENGTH][MAXLENGTH];
-	for(int x=0;x<MAXLENGTH;x++){
-		for(int y=0; y<MAXCOLUMNS;y++){
-			fscanf(fp,"%d",&numbs[x][y]);
-		}
+		case 2:
+		return num1-num2;
+		break;
+
+		case 3:
+		return num1*num2;
+		break;
+
+		case 4:
+		return num1/num2;
+		break;
 	}
+}
+void function5(int userchoice,int num1,int num2,double answer){
+	FILE *fp;
+	fp=fopen("results.txt","w");
+	switch(userchoice){
+		//Add
+		case 1:
+		printf("%d + %d = %f",num1,num2,answer);
+		fprintf(fp,"%d + %d = %f",num1,num2,answer);
+		break;
 
-	for(int y=0;y<MAXCOLUMNS;y++){
-		//create 1d arrays from a 2d a array here
-		printf("%d",mean(numbs));
+		//Subtract
+		case 2:
+		printf("%d - %d = %f",num1,num2,answer);
+		fprintf(fp,"%d - %d = %f",num1,num2,answer);
+		break;
+
+		//Multiply
+		case 3:
+		printf("%d * %d = %f",num1,num2,answer);
+		fprintf(fp,"%d * %d = %f",num1,num2,answer);
+		break;
+
+		//Divide
+		case 4:
+		printf("%d / %d = %f",num1,num2,answer);
+		fprintf(fp,"%d / %d = %f",num1,num2,answer);
+		break;
 	}
-
-
 	fclose(fp);
+}
+int main(){
+	int num1;
+	int num2;
+	int userchoice;
+	double answer;
+	char again;
+	bool doagain;
+	bool hewantsthenum;
+
+	hewantsthenum=true;
+	function1();
+	do{
+		doagain=false;
+		if(hewantsthenum){
+			num1=function2(1);
+			num2=function2(2);
+		}
+		userchoice=function3();
+		answer=function4(userchoice,num1,num2);
+		function5(userchoice,num1,num2,answer);
+		//use numbers again
+		while(true){
+			printf("\nWant to use the numbers again?(y/n)");
+			scanf(" %c",&again);
+			again=toupper(again);
+			if(again=='Y'){
+				doagain=true;
+				hewantsthenum=false;
+				break;
+			}
+			else if(again=='N'){
+				break;
+			}
+			else{
+				printf("\nNot a valid character");
+			}
+		}
+		//use new numbers if same numbers again is false
+		while(doagain!=true){
+			printf("\nWant to use new numbers?(y/n)");
+			scanf(" %c",&again);
+			again=toupper(again);
+			if(again=='Y'){
+				doagain=true;
+				hewantsthenum=true;
+			}
+			else if(again=='N'){
+				break;
+			}
+			else{
+				printf("\nNot a valid character");
+			}
+		}
+	}while(doagain);
 	system("pause");
 	return 0;
 }
